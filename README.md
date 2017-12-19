cc9a54dfd304f497ff293ba346bc59e8ae8f6a91
Melvyn Peignon <peignonmel@eisti.eu>
2017-12-19 16:34:00 +0100

    add a text blob analyser instead

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# toy_sentiment_API
+# About
+
+This Repository aim to build a Object Oriented Wrapper for the league of legends API.
+
+# Configuration
+
+To work on the project:
+
+Download the git repository:
+```
+git clone https://github.com/ADD_REPO_NAME
+```
+Enter into the repository:
+```
+cd ADD_REPO_NAME
+```
+
+Build the virtual environement:
+
+```
+virtualenv -p python3 venv
+```
+
+Activate your virtual environement:
+```
+source venv/bin/activate
+```
+
+Install the dependencies:
+```
+pip install -r requirements.txt
+```
--- sentiment_server.py
+++ sentiment_server.py
@@ -1,6 +1,10 @@
 import pickle
 from flask import Flask, jsonify, render_template, redirect, url_for, request
 from elasticsearch import Elasticsearch
+from textblob import Blobber
+from textblob.sentiments import NaiveBayesAnalyzer
+from nltk.corpus import movie_reviews
+import re
 
 app = Flask(__name__)
 
@@ -69,15 +73,28 @@ def label_a_tweet(tweet_id):
 
 @app.route('/predict', methods=['POST'])
 def predict_a_tweet():
-    vectorizer = pickle.load(open("models/vectorizer.p", 'rb'))
-    classifier = pickle.load(open("models/classifier.p", 'rb'))
     tweet_content = request.json["submit"]
-    to_predict = [tweet_content]
-    tfidf_to_predict = vectorizer.transform(to_predict)
-    predicted = classifier.predict(tfidf_to_predict)
-    print(tweet_content, predicted[0])
-    return jsonify(response=predicted[0])
+    cleaned_tweet = clean_text(tweet_content)
+    positive_likelyhood = text_blob(cleaned_tweet).sentiment.p_pos
+    if 0.45 <= positive_likelyhood <= 0.55:
+        return jsonify(sentiment="neutral", score=positive_likelyhood)
+    if positive_likelyhood > 0.60:
+        return jsonify(sentiment="positive", score=positive_likelyhood)
+    else:
+        return jsonify(sentiment="negative", score=positive_likelyhood)
+    #vectorizer = pickle.load(open("models/vectorizer.p", 'rb'))
+    #classifier = pickle.load(open("models/classifier.p", 'rb'))
+    #tweet_content = request.json["submit"]
+    #to_predict = [tweet_content]
+    #tfidf_to_predict = vectorizer.transform(to_predict)
+    #predicted = classifier.predict(tfidf_to_predict)
+    #print(tweet_content, predicted[0])
+
+
+def clean_text(tweet_to_clean):
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet_to_clean).split())
 
 
 if __name__ == '__main__':
+    text_blob = Blobber(analyzer=NaiveBayesAnalyzer())
     app.run(host='0.0.0.0', debug=True)
